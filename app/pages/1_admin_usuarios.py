5c9ff91c6fb0c1414aced71063bd61614d4cb3c5
Karmode <credcarmona@gmail.com>
2026-05-16 23:48:49 -0500

    Solucionar carga de las paginas de usuarios y roles

--- app/pages/1_admin_usuarios.py
+++ app/pages/1_admin_usuarios.py
@@ -1,10 +1,4 @@
 from app.pages_admin import admin_usuarios
 from app.core.sesion import obtener_sesion
 
-
-def main():
-    admin_usuarios.render(obtener_sesion())
-
-
-if __name__ == "__main__":
-    main()
+admin_usuarios.render(obtener_sesion())
